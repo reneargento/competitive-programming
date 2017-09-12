90e147c1d21312c834a0c1f296a99738a0e33082
rene-argento <rene.argento@gmail.com>
2017-09-11 21:44:22 -0300

    Added fast matrix exponentiation algorithm

--- src/com/br/algs/reference/algorithms/MatrixUtil.java
+++ src/com/br/algs/reference/algorithms/MatrixUtil.java
@@ -27,4 +27,54 @@ public class MatrixUtil {
         return matrix;
     }
 
+    //Fast exponentiation in mod 10^4
+    private static long mod = 10000;
+
+    //Used for matrices n x n
+    private static long[][] fastMatrixExponentiation(long[][] matrix, int exponent) {
+        if(exponent == 0) {
+            long[][] identityMatrix = new long[matrix.length][matrix[0].length];
+
+            for(int i = 0; i < matrix.length; i++) {
+                identityMatrix[i][i] = 1;
+            }
+
+            return identityMatrix;
+        }
+
+        if(exponent == 1) {
+            return matrix;
+        }
+
+        long[][] matrixSquared = matrixMultiplication(matrix, matrix);
+
+        if(exponent % 2 == 0) {
+            return fastMatrixExponentiation(matrixSquared, exponent / 2);
+        }
+
+        if(exponent % 2 == 1) {
+            return (matrixMultiplication(matrix, fastMatrixExponentiation(matrixSquared, exponent / 2)));
+        }
+
+        return matrix;
+    }
+
+    private static long[][] matrixMultiplication(long[][] matrix1, long[][] matrix2) {
+        long[][] result = new long[matrix1.length][matrix2[0].length];
+
+        for(int i = 0; i < matrix1.length; i++) {
+            for(int j = 0; j < matrix2[0].length; j++) {
+                long currentSum = 0;
+
+                for(int k = 0; k < matrix1.length; k++) {
+                    currentSum += matrix1[i][k] * matrix2[k][j];
+                }
+
+                result[i][j] = currentSum % mod;
+            }
+        }
+
+        return result;
+    }
+
 }
