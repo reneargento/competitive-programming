420a57c1d44fba15275f5e626ad1d54cad2ff3bc
Rene Argento <rene.argento@gmail.com>
2019-01-24 20:58:13 +0100

    Added n lg n lis algorithm

--- algorithms/dynamic/programming/LongestIncreasingSubsequence.java
+++ algorithms/dynamic/programming/LongestIncreasingSubsequence.java
@@ -1,95 +1,91 @@
 package algorithms.dynamic.programming;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
 /**
  * Created by Rene Argento on 28/12/16.
  */
-//Based on http://www.geeksforgeeks.org/dynamic-programming-set-3-longest-increasing-subsequence/
 
-import java.util.Stack;
-
-/**
- * The Longest Increasing Subsequence (LIS) problem is to find the length of the longest subsequence
- * of a given sequence such that all elements of the subsequence are sorted in increasing order.
- * For example, the length of LIS for
- * {10, 22, 9, 33, 21, 50, 41, 60, 80} is 6 and LIS is {10, 22, 33, 50, 60, 80}.
- */
+// Computes the longest increasing subsequence in O(N lg N)
+// Based on https://www.geeksforgeeks.org/construction-of-longest-monotonically-increasing-subsequence-n-log-n/
+// and https://www.geeksforgeeks.org/longest-monotonically-increasing-subsequence-size-n-log-n/
 public class LongestIncreasingSubsequence {
 
-    // O(n^2)
-    private static int longestIncreasingSubsequence(int[] sequence) {
-        //Base cases
-        if (sequence == null) {
-            return 0;
-        }
-        if (sequence.length == 1) {
-            return 1;
+    public static String longestIncreasingSubsequence(int[] array) {
+        if (array == null || array.length == 0) {
+            return "";
         }
 
-        int[] dp = new int[sequence.length];
-        int[] previous = new int[sequence.length];
+        int[] endIndexes = new int[array.length];
+        int[] previousIndices = new int[array.length];
 
-        for (int i = 0; i < dp.length; i++) {
-            dp[i] = 1;
-            previous[i] = Integer.MIN_VALUE;
-        }
+        Arrays.fill(previousIndices, -1);
+        int length = 1;
 
-        for (int i = 1; i < dp.length; i++) {
-            for (int j = 0; j < i; j++) {
-                if (sequence[j] < sequence[i]
-                        && dp[i] < dp[j] + 1) {
-                    dp[i] = dp[j] + 1;
-
-                    previous[i] = j; // Used to reconstruct sequence later
-                }
+        for (int i = 1; i < array.length; i++) {
+            // Case 1 - smallest end element
+            if (array[i] <= array[endIndexes[0]]) {
+                endIndexes[0] = i;
+            } else if (array[i] > array[endIndexes[length - 1]]) {
+                // Case 2 - highest end element - extends longest increasing subsequence
+                previousIndices[i] = endIndexes[length - 1];
+                endIndexes[length++] = i;
+            } else {
+                // Case 3 - middle end element
+                int indexToReplace = ceilIndex(array, endIndexes, -1, length - 1, array[i]);
+                previousIndices[i] = endIndexes[indexToReplace - 1];
+                endIndexes[indexToReplace] = i;
             }
         }
 
-        int longestIncSubsequenceLength = dp[0];
-        int elementIndexInSequence = 0; // Used to reconstruct sequence later
+        Deque<Integer> deque = new ArrayDeque<>();
 
-        for (int i = 0; i < dp.length; i++) {
-            if (dp[i] > longestIncSubsequenceLength) {
-                longestIncSubsequenceLength = dp[i];
-                elementIndexInSequence = i;
+        for (int i = endIndexes[length - 1]; i >= 0; i = previousIndices[i]) {
+            deque.push(array[i]);
+        }
+
+        StringBuilder longestIncreasingSubsequence = new StringBuilder();
+        while (!deque.isEmpty()) {
+            longestIncreasingSubsequence.append(deque.pop());
+
+            if (!deque.isEmpty()) {
+                longestIncreasingSubsequence.append(" ");
             }
         }
 
-        int currentNumberInSequence = sequence[elementIndexInSequence];
-        Stack<Integer> stack = new Stack<>();
-
-        System.out.print("Sequence found: ");
-        while (currentNumberInSequence != Integer.MIN_VALUE) {
-            stack.push(currentNumberInSequence);
-
-            elementIndexInSequence = previous[elementIndexInSequence];
-            if (elementIndexInSequence == Integer.MIN_VALUE) {
-                break;
-            }
-            currentNumberInSequence = sequence[elementIndexInSequence];
-        }
-
-        while (!stack.isEmpty()) {
-            System.out.print(stack.pop() + " ");
-        }
-
-        System.out.println();
-        return longestIncSubsequenceLength;
+        return longestIncreasingSubsequence.toString();
     }
 
-    public static void main(String[] args) {
-        int[] sequence1 = {3, 10, 2, 1, 20};
-        int[] sequence2 = {3, 2};
-        int[] sequence3 = {50, 3, 10, 7, 40, 80};
+    private static int ceilIndex(int[] array, int[] endIndexes, int low, int high, int key) {
+        while (high - low > 1) {
+            int middle = low + (high - low) / 2;
 
-        int longestSubsequence1 = longestIncreasingSubsequence(sequence1);
-        System.out.println("Expected sequence: 3, 10, 20\n");
-        int longestSubsequence2 = longestIncreasingSubsequence(sequence2);
-        System.out.println("Expected sequence: 3\n");
-        int longestSubsequence3 = longestIncreasingSubsequence(sequence3);
-        System.out.println("Expected sequence: 3, 10, 40, 80\n");
+            if (array[endIndexes[middle]] >= key) {
+                high = middle;
+            } else {
+                low = middle;
+            }
+        }
 
-        System.out.println("Longest subsequence 1: " + longestSubsequence1 + " Expected: 3");
-        System.out.println("Longest subsequence 2: " + longestSubsequence2 + " Expected: 1");
-        System.out.println("Longest subsequence 3: " + longestSubsequence3 + " Expected: 4");
+        return high;
     }
+
+    public static void main (String[] args) {
+        int[] array1 = {2, 5, 3, 7, 11, 8, 10, 13, 6};
+        System.out.println("Longest Increasing Subsequence: " + longestIncreasingSubsequence(array1));
+        System.out.println("Expected: 2 3 7 8 10 13");
+
+        int[] array2 = {6, 8, 1, 3, 9, 4, 5, 2, 7, -1, 11};
+        System.out.println("\nLongest Increasing Subsequence: " + longestIncreasingSubsequence(array2));
+        System.out.println("Expected: 1 3 4 5 7 11");
+
+        int[] array3 = {144, 2, 2, 106, 184, 21, 42, 68, 128, 189, 72, 108, 158, 98, 36, 8, 153, 48, 103, 133, 133, 48,
+                90, 154, 167, 146, 168, 129, 100, 46, 188, 197, 49, 190, 103, 33, 163, 97, 53, 92, 86, 125, 152, 196,
+                175, 188, 157, 129, 36, 60, 14, 121, 60, 104, 28, 27, 50, 148, 156};
+        System.out.println("\nLongest Increasing Subsequence: " + longestIncreasingSubsequence(array3));
+        System.out.println("Expected: 2 21 42 68 72 98 103 133 154 167 168 188 190 196");
+    }
+
 }
